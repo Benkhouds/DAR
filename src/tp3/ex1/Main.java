3490d1b79268d9c8c466a85872e817e356ff3737
hamza <hamza.benkhoud@etudiant-isi.utm.tn>
2021-11-27 20:18:35 +0100

    ex1

--- src/tp3/ex1/Main.java
+++ src/tp3/ex1/Main.java
@@ -1,19 +1,29 @@
 package tp3.ex1;
 
+import java.lang.Thread;
+
 public class Main {
-
-
-    // create a class that extends thread and another one that implements runnable
-    // they should print 'my thread is running' when running
-    // when both terminated, print 'it s over !!'
 
     public static void main(String[] args) {
 
+        ChildThread thread1 = new ChildThread();
+        Thread thread2 = new Thread(new RunnableThread());
+        thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            thread1.interrupt();
+            e.printStackTrace();
+        }
+        thread2.start();
+        try {
 
+            thread2.join();
+        } catch (InterruptedException e) {
+            thread2.interrupt();
+            e.printStackTrace();
+        }
+        System.out.println("it's over");
 
-        //output:
-        //my thread is running
-        //my thread is running
-        //it s over !!
     }
 }
